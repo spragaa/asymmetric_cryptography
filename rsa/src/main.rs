3fbd8c61e1a00a2929d3b40dca6b4185e069cc01
prumat4 <didukhmaksym4@gmail.com>
2024-12-05 19:47:36 +0200

    rm 0x in print

--- rsa/src/main.rs
+++ rsa/src/main.rs
@@ -3,7 +3,7 @@ use num_traits::Num;
 use rsa::RSA;
 
 fn hex_to_bigint(hex_str: &str) -> BigInt {
-    BigInt::from_str_radix(&hex_str.replace("0x", ""), 16)
+    BigInt::from_str_radix(&hex_str.replace("", ""), 16)
         .unwrap_or_else(|_| panic!("Invalid hex string: {}", hex_str))
 }
 
@@ -20,28 +20,28 @@ fn main() {
 
     println!("USER KEY INFORMATION:");
     println!("Public Exponent (E):");
-    println!("0x{:X}", user.public_key_e);
+    println!("{:X}", user.public_key_e);
     println!("\nPublic Modulus (N):");
-    println!("0x{:X}", user.public_key_n);
+    println!("{:X}", user.public_key_n);
     println!("\nPrivate Key (D):");
-    println!("0x{:X}", user.private_key_d);
+    println!("{:X}", user.private_key_d);
 
     print_separator();
 
     println!("SERVER PARAMETERS:");
     let server_e = BigInt::from(65537);
     let server_n = hex_to_bigint("95F964B34209E5616D806C33AE352C060EF68696FAD9692DCE59769937A8BDEF");
-    println!("Server Public Exponent (E): 0x{:X}", server_e);
-    println!("Server Public Modulus (N): 0x{:X}", server_n);
+    println!("Server Public Exponent (E): {:X}", server_e);
+    println!("Server Public Modulus (N): {:X}", server_n);
 
     print_separator();
 
     println!("MESSAGE ENCRYPTION TEST:");
-    let message = hex_to_bigint("ABC");
-    println!("Original Message: 0x{:X}", message);
+    let message = hex_to_bigint("deadbee");
+    println!("Original Message: {:X}", message);
 
     let encrypted = user.encrypt(&message, &server_e, &server_n);
-    println!("Encrypted Message: 0x{:X}", encrypted);
+    println!("Encrypted Message: {:X}", encrypted);
 
     print_separator();
 
@@ -57,20 +57,20 @@ fn main() {
     );
 
     println!("Encrypted Message to Decrypt:");
-    println!("0x{:X}", en_message);
+    println!("{:X}", en_message);
 
     let decrypted = user.decrypt(&en_message, &d, &n);
     println!("\nDecrypted Result:");
-    println!("0x{:X}", decrypted);
+    println!("{:X}", decrypted);
 
     print_separator();
 
     println!("KEY EXCHANGE TEST:");
     let (sent_key, sent_signature) = user.send_key(&message, &server_e, &server_n);
     println!("Sent Key:");
-    println!("0x{:X}", sent_key);
+    println!("{:X}", sent_key);
     println!("\nSent Signature:");
-    println!("0x{:X}", sent_signature);
+    println!("{:X}", sent_signature);
 
     print_separator();
 
